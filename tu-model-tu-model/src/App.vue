c4aa957fa95447ad8f09411a725f45e91ed4dcf6
Daniel Rataj <daniel.rataj@mall.cz>
2021-01-31 18:58:21 +0100

    Remove useless code

--- tu-model-tu-model/src/App.vue
+++ tu-model-tu-model/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <img alt="MALL Group IT" src="./assets/logo.png" width="100" class="m-auto mb-5" />
   <h1 class="text-center text-2xl text-black py-6 pt-0">Tu model, tu model</h1>
-  <!-- <div class="flex min-h-screen self-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8"> -->
   <div class="grid grid-cols-2">
-    <!-- <div class="p-8 w-1/2 bg-gray-50"> -->
     <div class="p-8 m-5 bg-gray-50">
       <UserRegistration
         v-model:username="username"
